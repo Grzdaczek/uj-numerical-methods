4ae364440c3f7be04135bb46b34fc374d4ad8a67
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-10-17 18:18:27 +0200

    Raport

--- num1/program.py
+++ num1/program.py
@@ -1,5 +1,14 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+	"pgf.texsystem": "pdflatex",
+	'font.family': 'serif',
+	'text.usetex': True,
+	'pgf.rcfonts': False,
+})
 
 def derive_a(x, h, fn):
 	return ( fn(x+h) - fn(x) ) / h
@@ -13,7 +22,7 @@ def abs_err(fn1, fn2, x):
 def plot_err(x, h, fn, Dfn, filename):
 	err_a = abs_err(lambda k: derive_a(k, h, fn), Dfn, x)
 	err_b = abs_err(lambda k: derive_b(k, h, fn), Dfn, x)
-	fig = plt.figure()
+	fig = plt.figure(figsize=(6, 3.75), dpi=600)
 	ax = fig.gca()
 	line_a, = ax.plot(h, err_a)
 	line_b, = ax.plot(h, err_b)
@@ -23,14 +32,16 @@ def plot_err(x, h, fn, Dfn, filename):
 	ax.set_yscale('log')
 	ax.legend()
 	plt.xlabel('log h')
-	plt.ylabel('log E(x)')
+	plt.ylabel('log E, dla x=0.3')
 	plt.grid()
 	fig.savefig(filename)
 
 x = np.float32(0.3)
 h = np.float32(np.logspace(start=-8, stop=-1, num=1000, endpoint=False))
+plot_err(x=0.3, h=h, fn=np.sin, Dfn=np.cos, filename='./f32.pgf')
 plot_err(x=0.3, h=h, fn=np.sin, Dfn=np.cos, filename='./f32.png')
 
 x = np.float64(0.3)
 h = np.float64(np.logspace(start=-16, stop=-1, num=1000, endpoint=False))
+plot_err(x=0.3, h=h, fn=np.sin, Dfn=np.cos, filename='./f64.pgf')
 plot_err(x=0.3, h=h, fn=np.sin, Dfn=np.cos, filename='./f64.png')
