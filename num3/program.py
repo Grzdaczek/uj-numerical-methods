ae19f5318da3e368e2c0cebf952d2da9bd876bb7
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-11-20 19:54:46 +0100

    Działający rozkład LU

---
+++ num3/program.py
@@ -0,0 +1,106 @@
+from functools import reduce
+
+class BandMatrix:
+	def __init__(self, bands) -> None:
+		self.bands = bands
+		self.width = len(bands)
+		diagonal = max(bands, key=lambda b: len(b))
+		self.size = len(diagonal)
+		self.diagonal_index = bands.index(diagonal)
+
+	def pos_to_band(self, i, j) -> tuple[float, float] or None:
+		band = j - i + self.diagonal_index
+		index = min(i, j)
+		if band < 0 or band > len(self.bands)-1:
+			return None
+		else:
+			return [band, index]
+	
+	def get(self, i, j) -> float:
+		ab = self.pos_to_band(i, j)
+		if ab:
+			a, b = ab
+			return self.bands[a][b]
+		else:
+			return 0
+
+	def set(self, i, j, x) -> None:
+		ab = self.pos_to_band(i, j)
+		if ab:
+			a, b, = ab
+			self.bands[a][b] = x
+
+	# def non_zero_before(self, i, j) -> tuple[int, int]:
+	# 	band = j - i + self.diagonal_index
+	# 	x = len(self.bands) - band
+	# 	return [0, 0]
+	# 	pass
+
+	# def non_zero_after(self, i, j) -> tuple[int, int]:
+	# 	return [0, 0]
+	# 	pass
+
+	# def first_meaningfull_j(i):
+	# 	pass
+
+	# def first_meaningfull_i(j):
+	# 	pass
+
+	def print(self):
+		for i in range(0, self.size):
+			line = ""
+			for j in range(0, self.size):
+				line += str(round(self.get(i, j), 4)) + "\t"
+			print(line)
+
+def lu_decomp(M):
+	for p in range(0, M.size):
+		upper_range = [[p, p + j] for j in range(0, M.size - p)]
+		lower_range = [[i + p, p] for i in range(1, M.size - p)]
+
+		for point in upper_range:
+			i, j = point
+			x = M.get(i, j) - sum([ M.get(i, k)*M.get(k, j) for k in range(0, i)])
+			M.set(i, j, x)
+
+		for point in lower_range:
+			i, j = point
+			x = M.get(i, j) - sum([ M.get(i, k)*M.get(k, j) for k in range(0, j)])
+			x *= 1 / M.get(j, j)
+			M.set(i, j, x)
+
+def lu_solve(vec):
+	pass
+
+def lu_det(M):
+	result = 1
+	for i in range(0, M.size):
+		result *= M.get(i, i)
+
+	return result
+
+N = 100
+bands = [
+	[0.2		for _ in range(0, N-1)],
+	[1.2		for _ in range(0, N)], # <--- diagonal
+	[0.1/(n+1)		for n in range(0, N-1)],
+	[0.4/(n+1)**2	for n in range(0, N-2)],
+]
+
+# bands = [
+# 	[8],
+# 	[4, 7],
+# 	[2, 3, 19],
+# 	[1, 8],
+# 	[3],
+# ]
+
+A = BandMatrix(bands)
+A.print()
+
+print('=========================')
+lu_decomp(A)
+A.print()
+
+print('=========================')
+print('det:', lu_det(A))
