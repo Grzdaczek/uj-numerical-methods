c34c8475f39489c72a481a597486219ab39ee640
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-11-28 16:54:47 +0100

    Wstęp do raportu

--- num4/program.py
+++ num4/program.py
@@ -3,59 +3,33 @@ import scipy as sp
 import scipy.linalg as spl
 import scipy.sparse as sps
 import scipy.sparse.linalg
-import matplotlib
-import matplotlib.pyplot as plt
-import time
 
-def calc(N):
-	# Na potrzeby wzoru Shermana-Morrisona
-	data = np.array([np.full(N, 9), np.full(N, 7)])
-	offsets = np.array([0, 1])
-	B = sps.dia_matrix((data, offsets), shape=(N, N))
-	u = np.full(N, 1)
-	v = u
+N = 50
 
-	# Wyraz wolny
-	b = np.full(N, 5)
+# Na potrzeby wzoru Shermana-Morrisona
+data = np.array([np.full(N, 9), np.full(N, 7)])
+offsets = np.array([0, 1])
+B = sps.dia_matrix((data, offsets), shape=(N, N))
+u = np.full(N, 1)
+v = u
 
-	# Macierz A = B+uv^T
-	A = B.toarray() + np.outer(u, v)
+# Wyraz wolny
+b = np.full(N, 5)
 
-	# Sprawdzenie, nie wykorzystujące struktury macierzy
-	start = time.time()
-	y1 = np.linalg.solve(A, b)
-	end = time.time()
-	t1 = end - start
+# Macierz A = B+uv^T
+A = B.toarray() + np.outer(u, v)
 
-	# Rozwiązanie z wykożystaniem wzoru Shermana-Morrisona
-	start = time.time()
-	LU = sps.linalg.splu(B.tocsc(), permc_spec='NATURAL')
-	p = LU.solve(b)
-	q = LU.solve(u)
-	y2 = p - (np.matmul(np.outer(q, v), p)) / (1 + np.dot(v, q))
-	end = time.time()
-	t2 = end - start
+# Sprawdzenie, nie wykorzystujące struktury macierzy
+y1 = np.linalg.solve(A, b)
 
-	tolerance = 6
-	roundarr = lambda arr: list(map(lambda x: round(x, tolerance), arr))
-	assert(roundarr(y1) == roundarr(y2))
+# Rozwiązanie z wykożystaniem wzoru Shermana-Morrisona
+LU = sps.linalg.splu(B.tocsc())
+p = LU.solve(b)
+q = LU.solve(u)
+y2 = p - (np.matmul(np.outer(q, v), p)) / (1 + np.dot(v, q))
 
-	return np.array([t1, t2])
+tolerance = 9
+roundarr = lambda arr: list(map(lambda x: round(x, tolerance), arr))
+assert(roundarr(y1) == roundarr(y2))
 
-# matplotlib.use("pgf")
-# matplotlib.rcParams.update({
-# 	"pgf.texsystem": "pdflatex",
-# 	'font.family': 'serif',
-# 	'text.usetex': True,
-# 	'pgf.rcfonts': False,
-# })
-
-space = np.logspace(2, 3.5, num=50, dtype=int)
-data = [calc(n) for n in space]
-fig = plt.figure()
-ax = fig.gca()
-# ax.set_ylim(0, 2)
-ax.set_ylabel('czas t[s]')
-ax.set_xlabel('rozmiar macierzy N')
-ax.plot(space, data)
-plt.show()
+print(y2)
