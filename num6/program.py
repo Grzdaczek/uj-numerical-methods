76fab1d88d54be09420d6da2d31e24ce8cf0e9b9
Grzdaczek <grzegorz.jnsk@gmail.com>
2021-12-20 04:24:11 +0100

    Wyniki w raporcie

--- num6/program.py
+++ num6/program.py
@@ -1,8 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import operator
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from numpy.polynomial import Polynomial
 from functools import reduce
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+	"pgf.texsystem": "pdflatex",
+	'font.family': 'serif',
+	'text.usetex': True,
+	'pgf.rcfonts': False,
+})
 
 def polynomial_fit(x, y, deg):
 	def phi(j):
@@ -17,7 +26,7 @@ def chart(f, dist, filename):
 	fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(
 		nrows=2, 
 		ncols=2,
-		figsize=(6, 6),
+		figsize=(6, 3.6),
 		dpi=600,
 		sharex=True,
 		sharey=True
@@ -34,12 +43,13 @@ def chart(f, dist, filename):
 		ax.plot(x, f(x), color='gray', linestyle='dashed', linewidth=1)
 		ax.plot(x, p(x), linewidth=1)
 
-	plot(ax1, 3)
-	plot(ax2, 7)
-	plot(ax3, 13)
-	plot(ax4, 25)
+	plot(ax1, 5)
+	plot(ax2, 8)
+	plot(ax3, 17)
+	plot(ax4, 28)
 	
-	plt.savefig(filename)
+	fig.tight_layout()
+	fig.savefig(filename)
 
 f1 = lambda x: 1.0 / (1.0 + (25 * x**2))
 f2 = lambda x: 1.0 / (1.0 + x**2)
@@ -47,7 +57,7 @@ f2 = lambda x: 1.0 / (1.0 + x**2)
 uniform = lambda n: np.linspace(-1, 1, n)
 cosine =lambda n: np.cos([(np.pi*(2*i+1))/(2*n) for i in range(n)])
 
-chart(f1, uniform, "f1_u.png")
-chart(f1, cosine, "f1_c.png")
-chart(f2, uniform, "f2_u.png")
-chart(f2, cosine, "f2_c.png")
+chart(f1, uniform, "f1_u.pgf")
+chart(f1, cosine, "f1_c.pgf")
+chart(f2, uniform, "f2_u.pgf")
+chart(f2, cosine, "f2_c.pgf")
